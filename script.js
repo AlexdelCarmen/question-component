241dc3d0f4a6ede899acec21dd193eeb2836c330
Cyrborg Werewolf <alexbdelcarmen@gmail.com>
2023-03-05 10:11:58 -0600

    Correct transitions and animations.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let newButtonContent = '-'
 
 buttons.forEach(button => {
     button.addEventListener('click', (event) => {
+        event.preventDefault();
         let currentCard = document.getElementById(button.dataset.number); 
         currentCard.classList.toggle('hide');
         let currentButtonContent = button.textContent; 
