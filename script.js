69cc36af1fc164c1a8be9aac00e7784d83d63343
Cyrborg Werewolf <alexbdelcarmen@gmail.com>
2023-03-04 20:49:43 -0600

    Add styles and script for buttons.

---
+++ script.js
@@ -0,0 +1,12 @@
+const buttons = document.querySelectorAll('button'); 
+let newButtonContent = '-'
+
+buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        let currentCard = document.getElementById(button.dataset.number); 
+        currentCard.classList.toggle('hide');
+        let currentButtonContent = button.textContent; 
+        button.textContent = newButtonContent; 
+        newButtonContent = currentButtonContent;
+    })
+})
